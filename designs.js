cb2913786707002375b774db21646a1908badc31
Thaddeus Fornal <56899514+judesDad@users.noreply.github.com>
2019-11-17 08:29:58 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,17 +1,36 @@
 // Select color input
-var colorSelection = document.getElementById('colorPicker');
+var color = document.getElementById('colorPicker');
 // Select size input
-var sizeForm = document.getElementById('sizePicker');
+var gridSize = document.getElementById('sizePicker');
+var designCanvas = document.getElementById('pixelCanvas');
 var gridHeight = document.getElementById('inputHeight');
 var gridWidth = document.getElementById('inputWidth');
 
 // When size is submitted by the user, call makeGrid()
-sizePicker.addEventListener('click', event => {
+function gridSize(sizePicker) {
+    document.getElementById(sizePicker).submit(event);
+    sizePicker.addEventListener('click', event => {
     event.preventDefault();
+    height = ('inputHeight.value');
+    width = ('inputWidth.value');
+    makeGrid(height, width);
 });
-console.log(event + 'test');
-function makeGrid() {
-
+    
+}
+console.log(event + 'works');
+console.log(gridHeight.value, gridWidth.value);
+console.log(color.value);
 // Your code goes here!
+function makeGrid(height, width) {
+    var designCanvas = document.getElementById('pixelCanvas') [0];
+    for (var i = 1; i <= height; i++) {
+        var row = document.createElement('tr');
+        for (var j = 1; j <= width; j++) {
+            var cell = document.createElement('td');
+            row.appendChild(cell);
+        }
+    }
+
+
 
 }
