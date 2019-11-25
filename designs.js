19059843fffc8243b8db8a6b9605ac397b598070
Thaddeus Fornal <56899514+judesDad@users.noreply.github.com>
2019-11-24 22:49:58 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,36 +1,39 @@
 // Select color input
-var color = document.getElementById('colorPicker');
+const color = document.getElementById("colorPicker");
+
 // Select size input
-var gridSize = document.getElementById('sizePicker');
-var designCanvas = document.getElementById('pixelCanvas');
-var gridHeight = document.getElementById('inputHeight');
-var gridWidth = document.getElementById('inputWidth');
-
+const size = document.getElementById("sizePicker");
+const gridHeight = document.getElementById("inputHeight");
+const gridWidth = document.getElementById("inputWidth");
+const canvas = document.getElementById("pixelCanvas");
 // When size is submitted by the user, call makeGrid()
-function gridSize(sizePicker) {
-    document.getElementById(sizePicker).submit(event);
-    sizePicker.addEventListener('submit', event => {
+document.addEventListener('submit', (event) => {
     event.preventDefault();
-    height = ('inputHeight.value');
-    width = ('inputWidth.value');
+    console.log("submit is successful");
+    height = (inputHeight.value);
+    width = (inputWidth.value);
+    console.log(height, width);
     makeGrid(height, width);
-});
+})
+
+
     
-}
-console.log(event + 'works');
-console.log(gridHeight.value, gridWidth.value);
-console.log(color.value);
-// Your code goes here!
+
+
+
 function makeGrid(height, width) {
-    var designCanvas = document.getElementById('pixelCanvas') [0];
-    for (var i = 1; i <= height; i++) {
-        var row = document.createElement('tr');
-        for (var j = 1; j <= width; j++) {
-            var cell = document.createElement('td');
-            row.appendChild(cell);
-        }
+
+for (let i = 0; i <= height; i++) {
+    let row = canvas.insertRow(i);
+    for (let j = 0; j <= width; j++) {
+        let cell = row.insertCell(j);
     }
+}    
+    
+    
+    }
+    
+
+// Your code goes here!
 
 
-
-}
