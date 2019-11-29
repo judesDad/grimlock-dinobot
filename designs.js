ba8c669f001867cf096ac59ce01a083f634eb313
Thaddeus Fornal <56899514+judesDad@users.noreply.github.com>
2019-11-28 21:21:39 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -14,9 +14,9 @@ document.addEventListener('submit', (event) => { // submit event to send height 
     event.preventDefault(); // used to stop the submit button from resetting the page
     //console.log("submit is successful");
     height = (inputHeight.value); // Value for Grid Height
-    width = (inputWidth.value);   // Value for Grid Width
+    width = (inputWidth.value); // Value for Grid Width
     //console.log(height, width);
-    designCanvas.innerHTML = "";  // Used to stop from adding rows and cells when submit button is used to  reset grid
+    designCanvas.innerHTML = ""; // Used to stop from adding rows and cells when submit button is used to  reset grid
     makeGrid(height, width); // calling make grid function for the submit event
 })
 
@@ -24,25 +24,26 @@ document.addEventListener('submit', (event) => { // submit event to send height 
 // Your code goes here!
 
 
-function makeGrid(height, width) {  // makeGrid Function to build grid by height and width
-
-for (let d = 0; d < height; d++) {  // nested for loop to to insert rows and cells for grid
-    let row = designCanvas.insertRow(d);
-    for (let j = 0; j < width; j++) {
-        let cell = row.insertCell(j);
-    cell.addEventListener('click', (event) => { // event listener click to set the color when a cell is clicked
-        event.target.style.backgroundColor = color.value;
-    designCanvas.addEventListener('dblclick', (event) => { // event listener double click to remomve color when cell is double clicked
-        event.target.style.backgroundColor = ''
-    });
-
-
-    })
+function makeGrid(height, width) { // makeGrid Function to build grid by height and width
+    
+    for(let d = 0; d < height; d++) { // nested for loop to to insert rows and cells for grid
+        let row = designCanvas.insertRow(d);
+        for(let j = 0; j < width; j++) {
+            let cell = row.insertCell(j);
+            cell.addEventListener('click', (event) => { // event listener click to set the color when a cell is clicked
+                event.target.style.backgroundColor = color.value;
+                designCanvas.addEventListener('dblclick', (event) => { // event listener double click to remomve color when cell is double clicked
+                    event.target.style.backgroundColor = ''
+                });
+                
+                
+            })
+        }
     }
+    
+    
 }
- 
- 
-    }
+
     
 
 
